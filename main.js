6e01140e249b3552babe73fc9e098838f7f7b5fb
ykrtz <122467146+ykrtz@users.noreply.github.com>
2024-01-26 18:17:27 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 
