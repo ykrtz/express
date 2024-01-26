fff006571d44ed93bd3ef0e64fe6ff29a027df51
ykrtz <122467146+ykrtz@users.noreply.github.com>
2024-01-26 18:11:03 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-const web3 = require('@solana/web3.js');
-const { TOKEN_PROGRAM_ID } = require('@solana/spl-token');
-const { exec } = require('child_process');
+
 
 // Enable CORS for all routes
 app.use(cors());
@@ -15,48 +12,6 @@ app.use(bodyParser.json());
 const mongoUri = 'mongodb+srv://yannickkurtzg:Yannick1996!@cluster0.00rei2t.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-  if (err) {
-    console.error('Error connecting to MongoDB', err);
-    return;
-  }
-  const collection = client.db("Dexscreener").collection("Addresses");
-
-  app.post('/api/saveToken', async (req, res) => {
-    const baseTokenAddress = req.body.baseTokenAddress;
-
-    collection.findOne({ baseTokenAddress: baseTokenAddress }, async (err, result) => {
-      if (err) {
-        res.status(500).send('Error querying database');
-        return;
-      }
-      if (result) {
-        res.status(200).send('Token already saved');
-      } else {
-        exec(`node index.js ${baseTokenAddress}`, (error, stdout, stderr) => {
-          if (error) {
-            console.error(`Error executing script: ${error}`);
-            return res.status(500).send('Error fetching metadata');
-          }
-
-          let metadata;
-        
-            metadata = stdout;
-        
-
-          // Save token with metadata
-          collection.insertOne({ baseTokenAddress, metadata }, (err, result) => {
-            if (err) {
-              res.status(500).send('Error saving to database');
-              return;
-            }
-            res.status(200).send('Token saved successfully with metadata');
-          });
-        });
-      }
-    });
-});
-  
 
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
