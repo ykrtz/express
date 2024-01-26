f1d4c489c0fc04f7a209ede1f138b5178559f96e
ykrtz <122467146+ykrtz@users.noreply.github.com>
2024-01-26 20:43:25 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-
-
 // Enable CORS for all routes
 app.use(cors());
 
@@ -12,6 +10,11 @@ app.use(bodyParser.json());
 
 const mongoUri = 'mongodb+srv://yannickkurtzg:Yannick1996!@cluster0.00rei2t.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 client.connect(err => {
   if (err) {
     console.error('Error connecting to MongoDB', err);
@@ -22,24 +25,15 @@ client.connect(err => {
   app.post('/api/saveToken', async (req, res) => {
     const baseTokenAddress = req.body.baseTokenAddress;
 
- 
-        
-
-          // Save token with metadata
-          collection.insertOne({ baseTokenAddress }, (err, result) => {
-            if (err) {
-              res.status(500).send('Error saving to database');
-              return;
-            }
-            res.status(200).send('Token saved successfully with metadata');
-          });
-        });
+    collection.insertOne({ baseTokenAddress, metadata }, (err, result) => {
+      if (err) {
+        res.status(500).send('Error saving to database');
+        return;
       }
+      res.status(200).send('Token saved successfully with metadata');
     });
+});
+  
 
 
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-
+});
